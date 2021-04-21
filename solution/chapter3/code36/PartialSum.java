4a97a5c1d6a14d25a242197542b9ad6227746e52
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2021-04-21 23:06:48 +0800

    first commit

---
+++ solution/chapter3/code36/PartialSum.java
@@ -0,0 +1,12 @@
+package chapter3.code36;
+
+public class PartialSum {
+  private boolean main(int N, int W) {
+    return false;
+  }
+
+  public static void main(String[] args) {
+    PartialSum partialSum = new PartialSum();
+    System.out.println(partialSum.main(8, 20));
+  }
+}
